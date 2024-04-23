8909199e083dfc913c48bff199dacfc7c72efa1b
srpromax <andersonbdavila00@gmail.com>
2024-04-22 21:01:56 -0500

    solucionando problemas del state

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,15 @@ function App() {
   const [indiceProceso, setIndiceProceso] = useState(1);
   const [peso, setPeso] = useState(1);
 
-  const [bloques, setBloques] = useState<Memoria[][]>([]);
-
 
   useEffect(() => {
-    const cantidad = procesos.reduce((acc, proceso) => proceso.estado === ESTADO.EXE ? acc + proceso.size : 0, 0);
-    
+    const cantidad = procesos.reduce((acc, proceso) => proceso.estado === ESTADO.EXE ? acc + proceso.size : acc, 0);
+
     setMemoriaDisponible(100 - cantidad);
-  }, [procesos])
+  }, [procesos, asignaciones])
 
-  useEffect(() => {
-    setBloques(dividirEnBloques(memoria));
-  }, [memoria])
- 
   const agregarProceso = () => {
+    if(peso <= 0) return;
 
     const proceso: Proceso = {
       nombre: `P${indiceProceso}`,
@@ -47,59 +42,60 @@ function App() {
     setProcesos([...procesos, proceso]);
   }
 
-  const asignarMemoria = (proceso: Proceso) => {
-    setMemoria(prevMemoria => {
-      const nuevaMemoria: Memoria[] = [...prevMemoria];
+  const asignarMemoria = (proceso: Proceso, memoriaActual: Memoria[] = memoria) => {
+    const nuevaMemoria: Memoria[] = [...memoriaActual];
       
-      let contador = 0;
-      let inserciones = 0;
-  
-      for (let i = 0; i < nuevaMemoria.length; i++) {
-        
-        if (nuevaMemoria[i].nombreProceso === "") {
-          contador++;
+    let contador = 0;
+    let inserciones = 0;
 
-          const procesoMemoria: Memoria = {
-            nombreProceso: proceso.nombre,
-            color: COLORES[indiceProceso]
-          }
+    for (let i = 0; i < nuevaMemoria.length; i++) {
+      
+      if (nuevaMemoria[i].nombreProceso === "") {
+        contador++;
 
-          nuevaMemoria[i] = procesoMemoria;
-  
-          if (nuevaMemoria[i + 1]?.nombreProceso !== "" || (contador === proceso.size)) {
-            
-            const inicio = i - inserciones;
-            
-            setAsignaciones(prevAsignaciones => {
-              return [...prevAsignaciones, {
-                inicio,
-                fin: i,
-                proceso: proceso.nombre
-              }]
-            })
-  
-            inserciones = 0;
-            
-          }
-  
-          inserciones++;
+        const procesoMemoria: Memoria = {
+          nombreProceso: proceso.nombre,
+          color: COLORES[indiceProceso]
         }
-  
-        if (contador === proceso.size) break;
+
+        nuevaMemoria[i] = procesoMemoria;
+
+        if (nuevaMemoria[i + 1]?.nombreProceso !== "" || (contador === proceso.size)) {
+          
+          const inicio = i - inserciones;
+
+
+          setAsignaciones(prevstate => {
+            return [...prevstate, {
+              inicio,
+              fin: i,
+              proceso: proceso.nombre
+            }]
+          });
+
+          inserciones = 0;
+          
+        }
+
+        inserciones++;
       }
-  
-      return nuevaMemoria;
-    });
+
+      if (contador === proceso.size) break;
+    }
+
+    setMemoria(nuevaMemoria);
   }
   
 
   const eliminarProceso = (nombreProceso: string, procesoSize: number) => {
 
     const procesosFiltrados = procesos.filter(proceso => proceso.nombre !== nombreProceso);
+    
     const asignacionesFiltradas = asignaciones.filter(asignacion => asignacion.proceso !== nombreProceso);
     const nuevaMemoria = [...memoria]
+    const cantidad = procesosFiltrados.reduce((acc, proceso) => proceso.estado === ESTADO.EXE ? acc + proceso.size : acc, 0);
+    const nuevaMemoriaDisponible = 100 - cantidad;
 
-    
     let contador = 0;
     
     for (let i = 0; i < nuevaMemoria.length; i++) {
@@ -114,15 +110,18 @@ function App() {
       }
     }  
 
+    setMemoriaDisponible(nuevaMemoriaDisponible);
     setMemoria(nuevaMemoria);
     setAsignaciones(asignacionesFiltradas);
+
     
     const procesosEnEspera = procesosFiltrados.filter(proceso => proceso.estado === ESTADO.WAIT);
     
+
     for (let i = 0; i < procesosEnEspera.length; i++) {
 
-      if(procesosEnEspera[i].size <= memoriaDisponible) {
-        asignarMemoria(procesosEnEspera[i]);
+      if(procesosEnEspera[i].size <= nuevaMemoriaDisponible) {
+        asignarMemoria(procesosEnEspera[i], nuevaMemoria);
         procesosEnEspera[i].estado = ESTADO.EXE;
       }
     }
@@ -193,7 +192,7 @@ function App() {
         <div
           className="flex flex-col gap-3 items-center pb-10"
         >
-          {bloques.map((bloque, index) => (
+          {dividirEnBloques(memoria).map((bloque, index) => (
             <div
             key={index}
             className="flex flex-col gap-3"
@@ -205,7 +204,8 @@ function App() {
                       className="h-6 w-6 md:h-8 md:w-8 xl:w-10 xl:h-10 2xl:h-14 2xl:w-14 flex 
                       items-center justify-center border-gray-400 border-2 text-white"
                       style={{backgroundColor: proceso.color}}
-                    >{proceso.nombreProceso}</div>
+                    >{proceso.nombreProceso}
+                    </div>
                 ))}
               </div>
         
